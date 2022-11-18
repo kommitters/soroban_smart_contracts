d2d3bf1a645117f52cbf5c312adfbc154d21d372
Lorenzo Zuluaga <l.zuluaga94@gmail.com>
2022-11-18 06:40:33 -0500

    Add basic behaviors for single donation and include test

--- cascading_donations/src/lib.rs
+++ cascading_donations/src/lib.rs
@@ -1,6 +1,6 @@
 #![no_std]
 
-use soroban_sdk::{contractimpl, contracttype, Env, BigInt, AccountId, Symbol, Vec};
+use soroban_sdk::{contractimpl, contracttype, Env, BigInt, AccountId, BytesN, Vec, ConversionError, Symbol, Address};
 
 use soroban_auth::{Identifier, Signature};
 
@@ -12,48 +12,116 @@ mod token {
 #[contracttype]
 pub enum DataKey {
     Name,
-    NodeId,
+    Admin,
+    TContract,
     ChildNodes // Vec<Node>
 }
 
 #[derive(Clone)]
 #[contracttype]
-pub enum Node {
-    Name,   // 10 character descriptive name
-    Address,    // Stellar public key(AccountId) or Contract ID (BytesN<32>)
-    Percentage  // Corresponding percentage of the donation
+pub struct Node {
+    name: Symbol,   // 10 character descriptive name
+    address: Address,    // Stellar public key(AccountId) or Contract ID (BytesN<32>)
+    percentage: u32  // Corresponding percentage of the donation
 }
 
-fn set_children(env: &Env) {
-
+// CHILDREN
+fn get_children(env: &Env) -> Vec<Node> {
+    let key = DataKey::ChildNodes;
+    env.data().get(key).unwrap().unwrap()
 }
+
+fn set_children(env: &Env, new_children: &Vec<Node>) {
+    env.data().set(DataKey::ChildNodes, new_children);
+}
+
+// TOKEN CONTRACT
+fn get_token_contract_id(env: &Env) -> BytesN<32> {
+    let key = DataKey::TContract;
+    env.data().get(key).unwrap().unwrap()
+}
+
+fn set_token_contract_id(e: &Env, token_id: &BytesN<32>) {
+    e.data().set(DataKey::TContract, token_id);
+}
+
+// ADMIN
+fn get_admin_id(env: &Env) -> Identifier {
+    let key = DataKey::Admin;
+    env.data().get(key).unwrap().unwrap()
+}
+
+fn set_admin_id(e: &Env, admin_id: &Identifier) {
+    e.data().set(DataKey::Admin, admin_id);
+}
+
+fn donate_to_child(env: &Env, node: &Node) {
+    // APPROVE SIGN
+
+    // XFER
+}
+
+fn apply_children_donations(env: &Env) {
+    /*
+    *  Iterate trhough the children, and:
+    *      if child.address == AccountId:
+    *           transfer -> AccountId, equivalent percentage
+    *      else (BytesN<32>):
+    *           Create instance of a CascadeDonationContract using the address
+    *           use the donation behavior to send the "amount" to the child contract
+    */
+    // let children = get_children(&env);
+
+    // for child in children {
+    //     match child {
+    //         Ok(node) => donate_to_child(env, &node),
+    //         Err(error) => panic!("Problem reading the node: {:?}", error),
+    //     }
+    // }
+}
+
+fn apply_donation(env: &Env, amount: &BigInt, donator: &Identifier) {
+    //Extract the "amount" from the donator account, into contract account
+
+    let tc_id = get_token_contract_id(&env);
+    let client = token::Client::new(&env, &tc_id);
+
+    let contract = env.current_contract();
+    let contract_identifier = Identifier::Contract(contract.clone());
+
+    client.xfer_from(
+        &Signature::Invoker,
+        &BigInt::zero(&env),
+        &donator,
+        &contract_identifier,
+        &amount
+    );
+
+    //apply_children_donations(&env);
+}
+
 pub struct CascadeDonationContract;
 
 pub trait CascadeDonationContractTrait {
-    fn initialize(env: Env);
-    fn donate(env: Env, amount: BigInt);
-    fn children(env: Env, new_children: Vec<Node>);
+    fn initialize(env: Env, tc_id: BytesN<32>, admin_id: Identifier, children: Vec<Node>);
+    fn donate(env: Env, amount: BigInt, donator: Identifier);
+    fn s_children(env: Env, new_children: Vec<Node>);
 }
 
 #[contractimpl]
 impl CascadeDonationContractTrait for CascadeDonationContract {
-    fn initialize(env: Env) {
+    fn initialize(env: Env, tc_id: BytesN<32>, admin_id: Identifier, children: Vec<Node>) {
+        set_token_contract_id(&env, &tc_id);
+        set_admin_id(&env, &admin_id);
+        set_children(&env, &children)
     }
 
-    fn donate(env: Env, amount: BigInt) {
-        /*
-        *  1. Extract the "amount" from the donator account, into contract account
-        *  2. Iterate trhough the children, and:
-        *      if child.address == AccountId:
-        *           transfer -> AccountId, equivalent percentage
-        *      else (BytesN<32>):
-        *           Create instance of a CascadeDonationContract using the address
-        *           use the donation behavior to send the "amount" to the child contract
-        */
+    fn donate(env: Env, amount: BigInt, donator: Identifier) {
+        apply_donation(&env, &amount, &donator);
     }
 
-    fn children(env: Env, new_children: Vec<Node>) {
-        set_children(&env);
+    fn s_children(env: Env, new_children: Vec<Node>) {
+        set_children(&env, &new_children);
     }
 }
 
--- cascading_donations/src/test.rs
+++ cascading_donations/src/test.rs
@@ -1,8 +1,87 @@
 #![cfg(test)]
 
+use super::{CascadeDonationContract, CascadeDonationContractClient, Identifier, Node};
+use soroban_sdk::{symbol, Env, testutils::{Accounts}, BigInt, IntoVal, BytesN, Vec, Symbol};
+use soroban_auth::{Signature, testutils::ed25519};
+
+use crate::token::{self, TokenMetadata};
+
 extern crate std;
+
+fn create_and_init_token_contract(env: &Env, admin_id: &Identifier) -> (BytesN<32>, token::Client) {
+    let token_id = env.register_contract_token(None);
+    let token_client = token::Client::new(&env, &token_id);
+
+    token_client.init(
+        &admin_id,
+        &TokenMetadata {
+            name: "Mmitkoin".into_val(&env),
+            symbol: "MTK".into_val(&env),
+            decimals: 7,
+        },
+    );
+
+    (token_id, token_client)
+}
 
 #[test]
 fn test() {
+    let env = Env::default();
 
+    // USERS
+    // let (admin_id, admin_sign) = ed25519::generate(&env);
+
+    // ADMIN
+    let admin = env.accounts().generate();
+    let admin_id = Identifier::Account(admin.clone());
+
+
+    // APPROVAL USER
+    let donator = env.accounts().generate();
+    let donator_id = Identifier::Account(donator.clone());
+
+    // CREATE OUR CUSTOM CONTRACT
+    let contract_id = env.register_contract(None, CascadeDonationContract);
+    let contract_client = CascadeDonationContractClient::new(&env, &contract_id);
+
+    // CREATE TOKEN CONTRACT
+    let (token_id, token_client) = create_and_init_token_contract(&env, &admin_id);
+
+    let children: Vec<Node> = [];
+
+    contract_client.initialize(&token_id, &admin_id);
+
+    // FUND DONATOR ACCOUNT
+    // let nonce = token_client.nonce(&admin_id);
+    // let approval_sign = ed25519::sign(
+    //     &env,
+    //     &admin_sign,
+    //     &token_id,
+    //     symbol!("mint"),
+    //     (&admin_id, &nonce, &admin_id, &BigInt::from_u32(&env, 2000)),
+    // );
+
+    // token_client.mint(admin, nonce, to, amount)
+
+    token_client.with_source_account(&admin).mint(
+        &Signature::Invoker,
+        &BigInt::zero(&env),
+        &donator_id,
+        &BigInt::from_u32(&env, 2000)
+    );
+
+    std::println!("======= DONATOR BALANCE ========: {:?}", token_client.balance(&donator_id));
+    std::println!("======= CONTRACT BALANCE ========: {:?}", token_client.balance(&Identifier::Contract(contract_id.clone())));
+    std::println!("=================================: {:?}", token_client.balance(&donator_id));
+
+    token_client.with_source_account(&donator).approve(
+        &Signature::Invoker,
+        &BigInt::zero(&env),
+        &Identifier::Contract(contract_id.clone()),
+        &BigInt::from_u32(&env, 1000)
+    );
+
+    contract_client.with_source_account(&donator).donate(&BigInt::from_u32(&env, 1000), &donator_id);
+    std::println!("======= DONATOR BALANCE ========: {:?}", token_client.balance(&donator_id));
+    std::println!("======= CONTRACT BALANCE ========: {:?}", token_client.balance(&Identifier::Contract(contract_id.clone())));
 }
